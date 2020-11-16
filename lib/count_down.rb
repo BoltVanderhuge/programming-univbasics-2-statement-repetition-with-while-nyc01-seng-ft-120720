d8e0a38d1092f744b067252be4459f931ba0ee08
John Wisneski <john.j.wisneski@gmail.com>
2020-11-16 20:25:27 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,11 @@
-# Write your code here
+hny = 0
+count = 10 
+
+if while count <= 10 && count != hny do
+  puts count
+  count = count -1
+end
+else 
+  puts "Happy New Year!"
+count = count -1
+end
